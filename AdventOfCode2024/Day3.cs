c0adcc5b2a8445e5c549aade4c37eb6fb08a8df1
MarioCake <weberlein.david+github@gmail.com>
2024-12-05 17:21:09 +0100

    Add Day 3 Part 1

---
+++ AdventOfCode2024/Day3.cs
@@ -0,0 +1,79 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode2024;
+
+[MemoryDiagnoser(false)]
+[ShortRunJob]
+public class Day3
+{
+    private readonly string _input = File.ReadAllText("day3.txt");
+
+    [Benchmark]
+    public int Part1()
+    {
+        ReadOnlySpan<char> inputSpan = _input.AsSpan();
+        MemoryExtensions.SpanSplitEnumerator<char> multiplicationCandidates = inputSpan.Split("mul(");
+
+        int sum = 0;
+        foreach (Range candidateRange in multiplicationCandidates)
+        {
+            ReadOnlySpan<char> candidate = inputSpan[candidateRange];
+            int endOfMultiplication = candidate.IndexOf(')');
+
+            if (endOfMultiplication == -1)
+            {
+                continue;
+            }
+
+            candidate = candidate[..endOfMultiplication];
+            int? firstNumber = null;
+
+            int firstNumberRange = 0;
+            foreach (char c in candidate)
+            {
+                if (char.IsDigit(c))
+                {
+                    firstNumberRange++;
+                    continue;
+                }
+
+                if (c == ',' && firstNumberRange > 0)
+                {
+                    firstNumber = int.Parse(candidate[..firstNumberRange]);
+                }
+
+                break;
+            }
+
+            if (!firstNumber.HasValue)
+            {
+                continue;
+            }
+
+            bool isNumber = true;
+            foreach (char c in candidate[(firstNumberRange + 1)..])
+            {
+                if (!char.IsDigit(c))
+                {
+                    isNumber = false;
+                    break;
+                }
+            }
+
+            if (!isNumber)
+            {
+                continue;
+            }
+
+            sum += firstNumber.Value * int.Parse(candidate[(firstNumberRange + 1)..]);
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    public int Part2()
+    {
+        return 0;
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -3,12 +3,12 @@ using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using Perfolizer.Horology;
 
-Console.WriteLine(new Day2().Part2());
+Console.WriteLine(new Day3().Part1());
 
 Console.WriteLine("Press any key to run benchmarks");
 Console.ReadLine();
 
-BenchmarkRunner.Run<Day2>(DefaultConfig.Instance
+BenchmarkRunner.Run<Day3>(DefaultConfig.Instance
     .WithSummaryStyle(DefaultConfig.Instance.SummaryStyle
         .WithTimeUnit(TimeUnit.Millisecond)
     )
