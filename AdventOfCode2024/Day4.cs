eb117a54892de70ce812c63da6c98dc76256dcb4
MarioCake <weberlein.david+github@gmail.com>
2024-12-07 00:30:39 +0100

    Add Day 4

---
+++ AdventOfCode2024/Day4.cs
@@ -0,0 +1,171 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AdventOfCode2024;
+
+[MemoryDiagnoser]
+[ShortRunJob]
+public class Day4
+{
+    private readonly string _input = File.ReadAllText("day4.txt");
+
+    [Benchmark]
+    public int Part1()
+    {
+        ReadOnlySpan<char> inputSpan = _input.AsSpan();
+        int rows = inputSpan.Count('\n') + 1;
+        int columns = inputSpan.IndexOf('\n') + 1;
+        int columnsEndOffset = columns - 4;
+        int rowsEndOffset = rows - 3;
+
+        int rowOffset1 = columns;
+        int rowOffset2 = columns * 2;
+        int rowOffset3 = columns * 3;
+        int xIndex = -1;
+
+        int sum = 0;
+        while (true)
+        {
+            int newIndex = inputSpan[(xIndex+1)..].IndexOf('X');
+            xIndex += newIndex + 1;
+            if (newIndex == -1)
+            {
+                return sum;
+            }
+            int column = xIndex % columns;
+
+            if (column < columnsEndOffset && inputSpan[xIndex..(xIndex + 4)] is "XMAS")
+            {
+                sum++;
+            }
+
+            if (column >= 3 && inputSpan[(xIndex-3)..(xIndex+1)] is "SAMX")
+            {
+                sum++;
+            }
+
+            int row = xIndex / columns;
+
+            if (row < rowsEndOffset &&
+                inputSpan[xIndex + rowOffset1] == 'M' &&
+                inputSpan[xIndex + rowOffset2] == 'A' &&
+                inputSpan[xIndex + rowOffset3] == 'S'
+            )
+            {
+                sum++;
+            }
+
+            if (row >= 3 &&
+                inputSpan[xIndex - rowOffset1] == 'M' &&
+                inputSpan[xIndex - rowOffset2] == 'A' &&
+                inputSpan[xIndex - rowOffset3] == 'S'
+            )
+            {
+                sum++;
+            }
+
+            if (row < rowsEndOffset && column < columnsEndOffset &&
+                inputSpan[xIndex + rowOffset1 + 1] == 'M' &&
+                inputSpan[xIndex + rowOffset2 + 2] == 'A' &&
+                inputSpan[xIndex + rowOffset3 + 3] == 'S'
+            )
+            {
+                sum++;
+            }
+
+            if (row >= 3 && column < columnsEndOffset &&
+                inputSpan[xIndex - rowOffset1 + 1] == 'M' &&
+                inputSpan[xIndex - rowOffset2 + 2] == 'A' &&
+                inputSpan[xIndex - rowOffset3 + 3] == 'S'
+            )
+            {
+                sum++;
+            }
+
+            if (row < rowsEndOffset && column >= 3 &&
+                inputSpan[xIndex + rowOffset1 - 1] == 'M' &&
+                inputSpan[xIndex + rowOffset2 - 2] == 'A' &&
+                inputSpan[xIndex + rowOffset3 - 3] == 'S'
+            )
+            {
+                sum++;
+            }
+
+            if (row >= 3 && column >= 3 &&
+                inputSpan[xIndex - rowOffset1 - 1] == 'M' &&
+                inputSpan[xIndex - rowOffset2 - 2] == 'A' &&
+                inputSpan[xIndex - rowOffset3 - 3] == 'S'
+            )
+            {
+                sum++;
+            }
+        }
+    }
+
+    [Benchmark]
+    public int Part2()
+    {
+        ReadOnlySpan<char> inputSpan = _input.AsSpan();
+        int rows = inputSpan.Count('\n') + 1;
+        int columns = inputSpan.IndexOf('\n') + 1;
+        int columnsEndOffset = columns - 3;
+        int rowsEndOffset = rows - 2;
+
+        int rowOffset1 = columns;
+        int rowOffset2 = columns * 2;
+        int mIndex = -1;
+
+        int sum = 0;
+        while (true)
+        {
+            int newIndex = inputSpan[(mIndex+1)..].IndexOf('M');
+            mIndex += newIndex + 1;
+            if (newIndex == -1)
+            {
+                return sum;
+            }
+            int x = mIndex % columns;
+            int y = mIndex / columns;
+
+            int bottomOffset = mIndex + rowOffset2;
+            if (y < rowsEndOffset && x < columnsEndOffset &&
+                inputSpan[mIndex + 2] == 'M' &&
+                inputSpan[mIndex + 1 + rowOffset1] == 'A' &&
+                inputSpan[bottomOffset] == 'S' && inputSpan[bottomOffset + 2] == 'S'
+               )
+            {
+                sum++;
+            }
+
+            if (y < rowsEndOffset && x < columnsEndOffset &&
+                inputSpan[mIndex + 2] == 'S' &&
+                inputSpan[mIndex + 1 + rowOffset1] == 'A' &&
+                inputSpan[bottomOffset] == 'M' && inputSpan[bottomOffset + 2] == 'S'
+            )
+            {
+                sum++;
+            }
+
+
+            int middleLeft = mIndex - 1;
+            int middleBottomLeftOffset = middleLeft + rowOffset1;
+            int bottomLeftOffset = bottomOffset - 2;
+            if (y < rowsEndOffset && x >= 2 &&
+                inputSpan[mIndex - 2] == 'S' &&
+                inputSpan[middleBottomLeftOffset] == 'A' &&
+                inputSpan[bottomOffset] == 'M' && inputSpan[bottomLeftOffset] == 'S'
+            )
+            {
+                sum++;
+            }
+
+            if (y >= 2 && x < columnsEndOffset &&
+                inputSpan[mIndex + 2] == 'M' &&
+                inputSpan[mIndex + 1 - rowOffset1] == 'A' &&
+                inputSpan[mIndex - rowOffset2] == 'S' && inputSpan[mIndex - rowOffset2 + 2] == 'S'
+               )
+            {
+                sum++;
+            }
+        }
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -3,12 +3,12 @@ using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using Perfolizer.Horology;
 
-Console.WriteLine(new Day3().Part2());
+Console.WriteLine(new Day4().Part2());
 
 Console.WriteLine("Press any key to run benchmarks");
 Console.ReadLine();
 
-BenchmarkRunner.Run<Day3>(DefaultConfig.Instance
+BenchmarkRunner.Run<Day4>(DefaultConfig.Instance
     .WithSummaryStyle(DefaultConfig.Instance.SummaryStyle
         .WithTimeUnit(TimeUnit.Millisecond)
     )
